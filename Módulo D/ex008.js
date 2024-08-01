7675166565c6f990ce6e67ee60339f4ad223dd0a
TSheep <mggodmine@gmail.com>
2024-08-01 18:18:20 -0300

    JS_estudo Módulo D v1.0
    Começando a brincar com condições o famoso if e else.

---
+++ Módulo D/ex008.js
@@ -0,0 +1,6 @@
+var vl1 = 150
+console.log(`A velocidade do seu veículo é ${vl1}Km/H.`)
+if (vl1 > 100){
+    console.log('Você foi MULTADO!')
+}
+console.log('Use cinto de segurança.')
---
+++ Módulo D/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Você mora nós ${país}.`)
+if (país == 'Brasil'){
+    console.log('Hue Hue BR!')
+} else {
+    console.log('Vem visitar o Brasil meu rei!')
+}
