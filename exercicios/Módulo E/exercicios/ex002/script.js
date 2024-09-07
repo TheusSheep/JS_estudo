d1efc66128018eb16d53b9b1d68851dbac848d4b
TSheep <mggodmine@gmail.com>
2024-09-07 09:24:31 -0300

    JS_estudo Módulo E v3.3.1
    Corrigindo alguns problemas da atividade e com preguiça de arrumar outros, como se alguém fosse ler isso.

--- exercicios/Módulo E/exercicios/ex002/script.js
+++ exercicios/Módulo E/exercicios/ex002/script.js
@@ -5,21 +5,19 @@ enviar.addEventListener('click', clique)
 // Definição das funções
 function clique(){
     let numero = document.getElementById('número')
-    let resposta = document.getElementById('tabuada')
+    let tab = document.getElementById('tab_s')
 
     if (numero.value.length == 0 ){
         window.alert('Número não encontrado para fazer a tabuada')
     } else {
-        let n1 = Number(numero.value)
-        let n2 = 0
-
-        resposta.innerHTML = `<h2>Tabuada<h2>`
+        let n = Number(numero.value)
+        tab.innerHTML = ""
 
         // Gerador da Tabuada
         for (var c = 1; c <= 10; c++){
-            resposta.innerHTML += ` ${n1} x ${c} = `
-            n2 = n1 * c
-            resposta.innerHTML += `${n2} <br>`
+           let item = document.createElement('option')
+           item.text = `${n} x ${c} = ${n*c}`
+           tab.appendChild(item)
         }
     }
 }
