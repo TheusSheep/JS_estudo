9e3fb0066590a1761821c202cc661b5ab154e242
TSheep <mggodmine@gmail.com>
2024-09-13 17:15:15 -0300

    JS_estudo Módulo F v3.4
    fazendo os exercícios aprendendo a usar array e o for...in, só isso mesmo é legal de usar.

---
+++ exercicios/Módulo F/aula 15/ambiente.js
@@ -0,0 +1,7 @@
+var ve = [2, 5, 3, 1, 9]
+
+console.log(`Esse é o array atual ${ve}`)
+ve.push(8)
+console.log(`adicionando 8 no array`)
+console.log(`Vetor organizado ${ve.sort()}`)
+console.log(`Quanidade de elementos: ${ve.length}`)
---
+++ exercicios/Módulo F/aula 15/vetor_na_tela.js
@@ -0,0 +1,5 @@
+var num = [4, 50, 10, 25]
+
+for (let c in num) {
+    console.log(`cada número do vetor: ${num[c]}`)
+}
