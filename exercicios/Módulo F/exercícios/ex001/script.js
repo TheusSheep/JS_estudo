35c61d061206efd6bb8a6f0daf36277a6d949a57
TSheep <mggodmine@gmail.com>
2024-09-26 17:03:11 -0300

    JS_estudo Módulo F v3.5.2ex
    Terminando os exercícios de JavaScript, percebi que meu código estava certo no começo, mas foi lerdo e apaguei ele inteiro e fiz de novo.

--- exercicios/Módulo F/exercícios/ex001/script.js
+++ exercicios/Módulo F/exercícios/ex001/script.js
@@ -1,19 +1,75 @@
 var adicionar = document.getElementById('ad')
 adicionar.addEventListener('click', clique)
 
-function clique(){
-    let tab_num = document.getElementById('tab_num')
-    let num_a = document.getElementById('num_a')
-    let list_num = []
-    
-    // Confirmação de validade
-    if(num_a.value <= 0 || num_a.value.length == 0 || num_a.value > 100){
-        window.alert('[ERRO] O número colocado não é permitido ou é inválido, por favor colocar número de 1 a 100')
+var finalizar = document.getElementById('fl')
+finalizar.addEventListener('click', fim)
+
+let tab = document.getElementById('tab_num')
+let num = document.getElementById('num_a')
+let resp = document.getElementById('resp')
+let list = []
+
+function confNum(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
     } else {
-        // Visualização na Tabela
+        return false
+    }
+}
+
+function confList(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+function clique(){
+    if(confNum(num.value) && !confList(num.value, list)){
+        list.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valor ${num_a.value} adicionado`
-        tab_num.appendChild(item)
+        item.text = `Valor ${num.value} adicionado`
+        tab.appendChild(item)
+        resp.innerHTML = ''
+    } else {
+        window.alert('[ERRO] Por favor, conferir de novo para ver se o número é válido ou se já está na lista')
     }
 
+    num.value = ''
+    num.focus
+}
+
+function fim(){
+    if(list.length == 0){
+        window.alert('[ERRO] Por favor colocar valor na lista')
+    } else {
+        let tot = list.length
+        let maior = list[0]
+        let menor = list[0]
+        let soma = 0
+        let med = 0
+
+        for(let pos in list){
+            if(list[pos] > maior){
+                maior = list[pos]
+            } else if(list[pos] < menor){
+                menor = list[pos]
+            }
+        }
+
+        for(let pos in list){
+            soma += list[pos]
+        }
+
+        med = soma / tot
+
+        resp.innerHTML = ''
+        resp.innerHTML += `<p>Tem ${tot} números na sua lista</p>`
+        resp.innerHTML += `<p>Maior valor na lista é ${maior}</p>`
+        resp.innerHTML += `<p>Menor valor na lista é ${menor}</p>`
+        resp.innerHTML += `<p>A soma dos números na lista é ${soma}</p>`
+        resp.innerHTML += `<p>A média dos números na lista é ${med}</p>`
+    }
+   
 }
